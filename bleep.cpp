e85bb70270a8c230899bcb2e0b3ba76b85230b57
Liberalist2 <tobias.zierl@gmx.de>
2022-11-17 21:36:36 +0100

    bleep

---
+++ bleep.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+
+std::string bleep(std::string word, std::string text) {
+  for (int i = 0; i < text.length(); i++) {
+    for (int j = 0; j <= word.length(); j++, i++) {
+        if (j == word.length()) {
+            for (;j >= 0; j--, i--){
+                text[i] = '*';
+            }
+            i += word.length();
+        } else {
+            if (word[j] != text[i]) {
+                break;
+            }
+        }
+    }
+  }
+  return text;
+}
+
+int main() {
+  std::string word = "broccoli";
+  std::string text = "I sometimes eat broccoli. There are three interesting things about broccoli. Number One. Nobody knows how to spell it. Number Two. No matter how long you boil it, it's always cold by the time it reaches your plate. Number Three. It's green. #broccoli";
+  std::cout << bleep(word,text)  << "\n";
+}
